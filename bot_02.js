a9d52854e2332690e314b9186e0e9ba5f05cea6a
Gabriela Klabunde <gabrielaklbunde2000@gmail.com>
2020-10-21 20:44:59 -0300

    Bot02 responder a usuário único

---
+++ bot_02.js
@@ -0,0 +1,29 @@
+const env = require('./.env')
+const Telegraf = require('telegraf')
+const bot = new Telegraf(env.token)
+
+bot.use(async(ctx, next) => {
+    const start = new Date()
+    await next()
+    const ms = new Date - start
+    const dataEHora = new Date().toLocaleString();
+    console.log(`${dataEHora} \n Tempo de Resposta: ${ms}ms`)
+})
+
+//Escuta do comando /start
+bot.start(async ctx =>{
+    const from = ctx.message.from
+    from.id = undefined
+    console.log(from)
+    if(from.username === 'gabrielaklabunde'){
+    await ctx.reply(`Olá ${from.username}, o seu nome é: ${from.first_name} ${from.last_name}!`)
+    }else{
+        await ctx.reply('Não estou autorizado a falar com estranhos')
+    }
+
+})
+
+//Evento de recepção de texto
+bot.on('text', ctx => ctx.reply('Alo Ha Mundo SENAI'))
+
+bot.launch()
